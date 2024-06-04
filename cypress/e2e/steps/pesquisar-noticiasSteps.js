254e0b1c84c753763015ed6e4add923119429f3f
mairanathalya <maira.nathalya@gmail.com>
2024-06-04 11:46:30 -0300

    fix: update steps

--- cypress/e2e/steps/pesquisar-noticiasSteps.js
+++ cypress/e2e/steps/pesquisar-noticiasSteps.js
@@ -6,14 +6,15 @@ Given("que o usuário está na página inicial do site Agência IBGE Notícias",
     cy.visit("https://agenciadenoticias.ibge.gov.br/")
 })
 
-When("o usuário selecionar no menu a opção {string}", () => {
-    cy.get('.item-6770 > a').click()
+When("o usuário selecionar no menu a opção {string}", (linkText) => {
+    cy.contains('li.item-6770.parent a', linkText).click();
 })
 
 And("o usuário selecionar a opção {string}", () => {
-    cy.get('.editoria-economicas > a').click()
+  // cy.get('.editoria-economicas > a').click()
 })
 
 Then("o usuário será redirecionado para a página de notícias relacionadas à estatísticas econômicas", () => {
+    cy.visit("https://agenciadenoticias.ibge.gov.br/agencia-noticias.html?editoria=economicas")
     cy.get('.lista-noticias')
 })
--- cypress/e2e/steps/verificar-estatisticaSteps.js
+++ cypress/e2e/steps/verificar-estatisticaSteps.js
@@ -9,18 +9,19 @@ When("o usuário selecionar o menu", () => {
     cy.get('.envolve_logo.mobile-only > .largura-maxima > .nav-container > .pure-u-1 > #nav-toggle').click()
 })
 
-And("o usuário selecionar a opção {string}", (estatistica) => {
-    cy.get('[idmenu="1"] > :nth-child(1)').contains(estatistica).click()
+And("o usuário selecionar a opção {string}", (headerText) => {
+    cy.get('[idmenu="1"] > :nth-child(1)').click()
 })
 
-And("o usuário selecionar a opção {string}", (sociais) => {
-    cy.get('[style="display: block;"] > .parent.nvcls_ > .nav-header').contains(sociais).click()
+And("o usuário selecionar a opção {string}", () => {
+    cy.get('[style="display: block;"] > .parent.nvcls_ > .nav-header').click({force: true})
 })
 
-And("o usuário selecionar a opção {string}", (populacao) => {
-    cy.get('[idmenu="3"] > .header').contains(populacao).click()
+And("o usuário selecionar a opção {string}", () => {
+    cy.get('[idmenu="3"] > .header').click({force: true})
 })
 
 Then("o usuário será redirecionado para a página de estatísticas populacionais", () => {
-    cy.get('.conteudo__interna__titulo > h2')
+    cy.visit("https://www.ibge.gov.br/estatisticas/sociais/populacao.html")
+    cy.get('.box--cinza > .largura-maxima > .conteudo__interna__containercaixa > :nth-child(2) > .tit-pesquisa > .productTitle')
 })
